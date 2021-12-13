aca59713f377607b0e6fadd82612e01b8d686e45
Alex Lukash <2hala2real2madrid2@gmail.com>
2021-12-13 20:04:48 +0300

    fixed StringToDouble+Parsing

--- StringToDouble+Parsing/src/Solution.java
+++ StringToDouble+Parsing/src/Solution.java
@@ -1,12 +1,34 @@
+import java.util.Objects;
 import java.util.Scanner;
+
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("Enter fractional number: ");
-        Scanner scanner = new Scanner();
-        double Double = java.lang.Double.parseDouble(string);
-        System.out.println(Math.round(Double));
 
+        ReciursiaEpta();
         //напишите тут ваш код
+    }
+
+    public static void ReciursiaEpta() {
+        String stop = "stop";
+        String stops = "Stop";
+        System.out.println("Enter fractional number: ");
+        Scanner scanner = new Scanner(System.in);
+        String scanners = scanner.nextLine();
+
+        if(!Objects.equals(scanners, stop) && !Objects.equals(scanners, stops)) {
+            try {
+                double Double = java.lang.Double.parseDouble(scanners);
+                System.out.println("Rounded number: " +  Math.round(Double));
+                ReciursiaEpta();
+            } catch (Exception e) {
+                System.out.println("Exception occurred");
+                ReciursiaEpta();
+            }
+        }
+            else
+            {
+            System.exit(0);
+        }
     }
 }
